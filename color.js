b4cc4ef21f76fe1f0c976d70ba4771211afc85ab
pravinb1703 <pravin.budhehalkar@gmail.com>
2025-03-08 15:41:10 +0530

    Add files via upload
    dom2

---
+++ color.js
@@ -0,0 +1,21 @@
+let red=document.getElementById('red')
+let htag=document.getElementById('htag')
+
+
+red.addEventListener('click',()=>{
+    htag.style.color='red'
+})
+
+green.addEventListener('click',()=>{
+    htag.style.color='green'
+})
+yellow.addEventListener('click',()=>{
+    htag.style.color='yellow'
+})
+
+blue.addEventListener('click',()=>{
+    htag.style.color='blue'
+})
+reset.addEventListener('click',()=>{
+    htag.style.color='black'
+})
